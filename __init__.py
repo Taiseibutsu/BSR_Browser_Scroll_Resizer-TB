76c633a3a43a3037efa620f26e66ec52f9f029eb
Taiseibutsu <84092569+Taiseibutsu@users.noreply.github.com>
2023-10-09 20:50:19 +0200

    Update for 4.0 changes

--- __init__.py
+++ __init__.py
@@ -10,6 +10,10 @@ bl_info = {
     "category": "TB"}
 
 import bpy 
+from bpy.types import AddonPreferences
+
+class TB_FBSR_custom_prop(bpy.types.PropertyGroup):
+    tb_multiplier_resize_factor: bpy.props.IntProperty(default=10)
 
 class TB_WheelUp(bpy.types.Operator):
     """Increase the size of file Viewer"""
@@ -18,20 +22,22 @@ class TB_WheelUp(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
+        wm = bpy.context.window_manager
+        cp = wm.TB_FBSR_custom_prop_wm
+        mrf = cp.tb_multiplier_resize_factor
+        
         type = context.space_data.params.display_type
         if type == 'LIST_VERTICAL':
             bpy.context.space_data.params.display_type = 'LIST_HORIZONTAL'               
         elif type == 'LIST_HORIZONTAL':
-            bpy.context.space_data.params.display_type = 'THUMBNAIL' 
-            bpy.context.space_data.params.display_size = 'TINY'          
+            bpy.context.space_data.params.display_type = 'THUMBNAIL'
+            bpy.context.space_data.params.display_size = 24      
         elif type == 'THUMBNAIL':
-            size = context.space_data.params.display_size
-            if size == 'TINY':      
-                bpy.context.space_data.params.display_size = 'SMALL'
-            elif size == 'SMALL':
-                bpy.context.space_data.params.display_size = 'NORMAL'
-            elif size == 'NORMAL':
-                bpy.context.space_data.params.display_size = 'LARGE'
+            if bpy.context.space_data.params.display_size < 256:
+                if (bpy.context.space_data.params.display_size + mrf) >= 256:
+                    bpy.context.space_data.params.display_size = 256
+                if bpy.context.space_data.params.display_size < 256:
+                    bpy.context.space_data.params.display_size = bpy.context.space_data.params.display_size + mrf
         return {'FINISHED'}
 class TB_WheelDown(bpy.types.Operator):
     """Decrease the size of file Viewer"""
@@ -40,21 +46,32 @@ class TB_WheelDown(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
+        wm = bpy.context.window_manager
+        cp = wm.TB_FBSR_custom_prop_wm
+        mrf = cp.tb_multiplier_resize_factor
         type = context.space_data.params.display_type
+
         if type == 'THUMBNAIL':
-            size = context.space_data.params.display_size
-            if size == 'LARGE':
-                bpy.context.space_data.params.display_size = 'NORMAL'
-            if size == 'NORMAL':
-                bpy.context.space_data.params.display_size = 'SMALL'
-            if size == 'SMALL':
-                bpy.context.space_data.params.display_size = 'TINY'
-            if size == 'TINY':      
+            if bpy.context.space_data.params.display_size > 16:
+                if (bpy.context.space_data.params.display_size - mrf) <= 16:
+                    bpy.context.space_data.params.display_size = 16
+                else:                    
+                    bpy.context.space_data.params.display_size = bpy.context.space_data.params.display_size - mrf
+            if bpy.context.space_data.params.display_size <= 16:
                 bpy.context.space_data.params.display_type = 'LIST_HORIZONTAL'
         if type == 'LIST_HORIZONTAL':
             bpy.context.space_data.params.display_type = 'LIST_VERTICAL'                                
         return {'FINISHED'}
-classes = (TB_WheelDown,TB_WheelUp)
+
+class TB_Custom_Dimensions_PreferencesPanel(AddonPreferences):
+    bl_idname = __name__
+    def draw(self, context):
+        layout = self.layout
+        box=layout.box()
+        cp = bpy.context.window_manager.TB_FBSR_custom_prop_wm
+        box.prop(cp,"tb_multiplier_resize_factor",text="Multiplier_Factor")
+
+classes = (TB_WheelDown,TB_WheelUp,TB_Custom_Dimensions_PreferencesPanel)
 def register():
  #CLASS
     for cls in classes:
@@ -70,6 +87,8 @@ def register():
         kmi.active = True
         kmi = km.keymap_items.new("tbcontext.filesizeincrease", 'WHEELINMOUSE', 'PRESS', alt=True)
         kmi.active = True
+    bpy.utils.register_class(TB_FBSR_custom_prop)
+    bpy.types.WindowManager.TB_FBSR_custom_prop_wm = bpy.props.PointerProperty(type=TB_FBSR_custom_prop)
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls) 
@@ -79,6 +98,8 @@ def unregister():
     kmi = km.keymap_items.new("tbcontext.filesizedecrease", 'WHEELOUTMOUSE', 'PRESS', alt=True)
     kmi.active = False
     kmi = km.keymap_items.new("tbcontext.filesizeincrease", 'WHEELINMOUSE', 'PRESS', alt=True)
-    kmi.active = False                   
+    kmi.active = False           
+    bpy.utils.unregister_class(TB_FBSR_custom_prop)
+    bpy.types.WindowManager.TB_FBSR_custom_prop = bpy.props.PointerProperty(type=TB_FBSR_custom_prop)        
 if __name__ == "__main__":
     register()
